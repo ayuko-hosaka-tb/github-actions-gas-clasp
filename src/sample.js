f553f94767434470b8647f6f45c5a1bc13386dbb
AyukoHosaka <ayuko_hosaka@trustbank.co.jp>
2024-12-06 11:16:35 +0900

    create: docs

--- src/sample.js
+++ src/sample.js
@@ -1,4 +1,7 @@
-function test() {
+/**
+ * コンソールにメッセージを表示する
+ */
+function consoleDescription() {
     console.log('GitHub Actions Deploy Automation!');
     console.log('Does it automate it?');
     console.log('No..');
